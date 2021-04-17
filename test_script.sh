5908d5ffa4bf665549d294bce7a0a1c44ccc5136
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2021-04-17 01:04:01 -0500

    again

--- test_script.sh
+++ test_script.sh
@@ -5,9 +5,12 @@ cd "$(dirname "$0")"
 
 . ./init.sh
 
+pg_ctl -D ${PGDATA} -l logfile start
+
 USE_PGXS=1 make installcheck
 
-# pg_ctl -D ${PGDATA} -l logfile stop
+pg_ctl -D ${PGDATA} -l logfile stop
+
 # USE_PGXS=1 make clean
 # rm -r ${PGDATA}
 
