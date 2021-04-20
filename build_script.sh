fd1e0671488fdcefd481021b75857a7526685dc9
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2021-04-19 22:41:36 -0500

    try first copy over from master_msys2_simplest_test_installcheck

---
+++ build_script.sh
@@ -0,0 +1,44 @@
+
+cd "$(dirname "$0")"
+
+. ./init.sh
+
+# set -v -x -e
+set -e
+
+# which R
+# /c/RINSTALL/bin/x64/R
+
+winpty -Xallow-non-tty initdb --username=${PGUSER} --pgdata="${PGDATA}" --auth=trust --encoding=utf8 --locale=C
+# Success. You can now start the database server using:
+# C:/msys64/mingw64/bin/pg_ctl -D C:/msys64//home/appveyor/mingw64/postgresql/Data -l logfile start
+# C:/msys64/mingw64/bin/pg_ctl -D ${PGDATA} -l logfile start
+
+# first
+pg_ctl -D ${PGDATA} -l logfile start
+pg_ctl -D ${PGDATA} -l logfile stop
+
+# do again
+pg_ctl -D ${PGDATA} -l logfile start
+pg_ctl -D ${PGDATA} -l logfile stop
+
+# leave it up
+pg_ctl -D ${PGDATA} -l logfile start
+
+winpty -Xallow-non-tty psql -c 'select version();'
+
+USE_PGXS=1 make
+
+USE_PGXS=1 make install
+
+winpty -Xallow-non-tty psql -c 'CREATE EXTENSION plr;'
+winpty -Xallow-non-tty psql -c 'SELECT plr_version();'
+winpty -Xallow-non-tty psql -c 'SELECT   r_version();'
+winpty -Xallow-non-tty psql -c 'DROP EXTENSION plr;'
+
+# must stop, else Appveyor job will hang.
+pg_ctl -D ${PGDATA} -l logfile stop
+
+
+# set +v +x +e
+set +e
---
+++ init.sh
@@ -0,0 +1,34 @@
+
+cd "$(dirname "$0")"
+
+# pwd
+# /c/projects/plr
+
+export R_HOME=$(cygpath "${R_HOME}")
+
+export APPVEYOR_BUILD_FOLDER=$(cygpath "${APPVEYOR_BUILD_FOLDER}")
+# echo $APPVEYOR_BUILD_FOLDER
+# /c/projects/plr
+
+export plrsource=${APPVEYOR_BUILD_FOLDER}
+
+# echo ${MINGW_PREFIX}
+# /mingw64
+export pginstall=${MINGW_PREFIX}
+
+export dirpostgresql=/postgresql
+
+# e.g., in the users home directory
+export TZ=UTC
+export PGAPPDIR="C:/msys64$HOME"${pginstall}/postgresql/Data
+export     PGDATA=${PGAPPDIR}
+export      PGLOG=${PGAPPDIR}/log.txt
+export PGLOCALDIR=${pginstall}/share${dirpostgresql}/
+# database params (default)
+export PGDATABASE=postgres
+export PGPORT=5432
+export PGUSER=postgres
+
+# not required in compilation
+#     required in "CREATE EXTENSION plr;" and regression tests
+export PATH=${R_HOME}/bin${R_ARCH}:$PATH
