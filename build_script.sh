addb8f536776c82eef4341475e2dc99efd058391
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2021-04-26 16:09:00 -0500

    again

--- build_script.sh
+++ build_script.sh
@@ -88,8 +88,8 @@ echo END MY pg_config
 # minimum (strange that this env variable PGDATABASE is explictly required)
 #
 #
-winpty -Xallow-non-tty initdb --username=appveyor --pgdata="${PGDATA}" --auth=trust --encoding=utf8 --locale=C
-winpty -Xallow-non-tty createdb --username=appveyor --owner=appveyor appveyor
+winpty -Xallow-non-tty initdb --pgdata="${PGDATA}" --auth=trust --encoding=utf8 --locale=C
+winpty -Xallow-non-tty createdb --owner=appveyor appveyor
 
 # Success. You can now start the database server using:
 # C:/msys64/mingw64/bin/pg_ctl -D C:/msys64//home/appveyor/mingw64/postgresql/Data -l logfile start
