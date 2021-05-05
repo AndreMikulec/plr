9371024e054dff4761ce937e33949a4c1c679692
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2021-05-04 20:10:05 -0500

    copy plr_transaction files from the HEAD of th branch master_andre_petere_plr_procedure-transaction

--- sql/plr_transaction.sql
+++ sql/plr_transaction.sql
@@ -1,22 +1,37 @@
 CREATE TABLE test1 (a int, b text);
 
-
-CREATE PROCEDURE transaction_test1()
-LANGUAGE plr
-AS $$
-for(i in 0:9){
-  pg.spi.exec(paste("INSERT INTO test1 (a) VALUES (", i, ")"))
-  if (i %% 2 == 0) {
-    pg.spi.commit()
-  } else {
-    pg.spi.rollback()
+CREATE OR REPLACE FUNCTION test_create_procedure_transaction() RETURNS void
+  AS
+$BODY$
+  version_11plus  <- pg.spi.exec("select current_setting('server_version_num')::integer >= 110000;")
+  if(version_11plus[[1]])
+  {
+    pg.spi.exec("
+      CREATE OR REPLACE PROCEDURE transaction_test1()
+        AS
+      $$
+        for(i in 0:9){
+          pg.spi.exec(paste('INSERT INTO test1 (a) VALUES (', i, ');'))
+          if (i %% 2 == 0) {
+            pg.spi.commit()
+          } else {
+            pg.spi.rollback()
+          }
+        }
+      $$ LANGUAGE plr;
+      ")
+    pg.spi.exec("CALL transaction_test1();")
   }
-}
-$$;
+  else
+  {
+    pg.spi.exec("INSERT INTO test1 (a) VALUES (0);")
+    pg.spi.exec("INSERT INTO test1 (a) VALUES (2);")
+    pg.spi.exec("INSERT INTO test1 (a) VALUES (4);")
+    pg.spi.exec("INSERT INTO test1 (a) VALUES (8);")
+  }
+$BODY$
+LANGUAGE plr;
 
-CALL transaction_test1();
+SELECT test_create_procedure_transaction();
 
 SELECT * FROM test1;
-
-
-TRUNCATE test1;
