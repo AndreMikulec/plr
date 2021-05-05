53b874e9788d4772cb4bf411efae55efda42815e
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2021-05-04 20:17:49 -0500

    dynamically call proc transaction_test1, based on postgres server_version_num

--- sql/plr_transaction.sql
+++ sql/plr_transaction.sql
@@ -3,7 +3,7 @@ CREATE TABLE test1 (a int, b text);
 CREATE OR REPLACE FUNCTION test_create_procedure_transaction() RETURNS void
   AS
 $BODY$
-  version_11plus  <- pg.spi.exec("select current_setting('server_version_num')::integer >= 110000;")
+  version_11plus  <- pg.spi.exec("SELECT current_setting('server_version_num')::integer >= 110000;")
   if(version_11plus[[1]])
   {
     pg.spi.exec("
@@ -20,7 +20,6 @@ $BODY$
         }
       $$ LANGUAGE plr;
       ")
-    pg.spi.exec("CALL transaction_test1();")
   }
   else
   {
@@ -33,5 +32,17 @@ $BODY$
 LANGUAGE plr;
 
 SELECT test_create_procedure_transaction();
+
+\o out.txt
+SELECT current_setting('server_version_num')::integer server_version_num;
+\o
+\gset
+
+\o out.txt
+SELECT CASE WHEN :server_version_num >= 110000 THEN 'CALL transaction_test1();' ELSE '' END thecall;
+\o
+\gset
+
+:thecall
 
 SELECT * FROM test1;
