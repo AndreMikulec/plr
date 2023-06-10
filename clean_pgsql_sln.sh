4360e83cece711181c846c54b5829ebf9665ac88
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2023-06-09 22:27:39 -0400

    FIX TO - if exist C:\projects\postgresql\pgsql.sln + BIND + COMPILE
    FIX TO - if exist C:\projects\postgresql\pgsql.sln if "%pggithubbincacheextracted%"=="false" if "%branchtagcommit%"=="yes"
    
    replacments
    
    "${env:pg}" -notmatch "[.]")
    "${env:branchtagcommit}" -eq "yes"
    
    "$env:pg" -match "[.]"
    "${env:branchtagcommit}" -eq "no"
    
    Back to the original CONTENTS of clean_pgsql_sln.sh

--- clean_pgsql_sln.sh
+++ clean_pgsql_sln.sh
@@ -2,14 +2,6 @@
 set -v -x -e
 # set -e
 
-#
-# Only when compiling pg from scratch.
-# If pg is not being compiled then pgsql.sln will not exist and not have been created.
-#
-# The semi-duplicate extra 'plr' entry in the pgsql.sln file 
-# prevents building if Microsoft Visual Studio is the version 2019 and greater.
-#
-
 pwd
 
 # ls -alrt /c/projects/plr
@@ -18,24 +10,16 @@ pwd
 export PGSLNLOCATION=/c/projects/postgresql/pgsql.sln
 echo PGSLNLOCATION: $PGSLNLOCATION
 
-if [ -f "${PGSLNLOCATION}" ]
-then
-  echo FILE ${PGSLNLOCATION} exists
-  echo REMOVE the semi-duplicate extra plr ENTRY from the file pgsql.sln
+# GUID to later delete
+export BRACEDGUID=`cat $PGSLNLOCATION | grep '"plr", "plr"' | grep -Po '"{[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+}"$' | grep -Po '{[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+}'`
 
-  # GUID to later delete
-  export BRACEDGUID=`cat $PGSLNLOCATION | grep '"plr", "plr"' | grep -Po '"{[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+}"$' | grep -Po '{[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+-[0-9A-Z]+}'`
+echo From pgsql.sln the extra BRACEDGUID to Remove: $BRACEDGUID
+  
+# remove GlobalSection(NestedProjects) entry GUID
+sed -i "/= $BRACEDGUID/d" $PGSLNLOCATION
 
-  echo From pgsql.sln the extra BRACEDGUID to Remove: $BRACEDGUID
-    
-  # remove GlobalSection(NestedProjects) entry GUID
-  sed -i "/= $BRACEDGUID/d" $PGSLNLOCATION
-
-  # remove entry of Project line (and its GUID) and its EndProject line
-  sed -i '/"plr", "plr"/,+1d' $PGSLNLOCATION
-else
-  echo FILE ${PGSLNLOCATION} does not exist
-fi
+# remove entry of Project line (and its GUID) and its EndProject line
+sed -i '/"plr", "plr"/,+1d' $PGSLNLOCATION
 
 set +v +x +e
 # set +e
