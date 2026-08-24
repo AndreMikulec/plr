e28cf962ce8e4557f0a0d3d27b7231215ca5e9ca
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-24 15:27:22 -0400

    echo 'host    postgres        runner          127.0.0.1/32            trust' | sudo tee /etc/postgresql/${PG}/main/pg_hba.conf > /dev/null

--- get_postgres_binaries_compiled_on_ubuntu_from_postgres.org.sh
+++ get_postgres_binaries_compiled_on_ubuntu_from_postgres.org.sh
@@ -55,8 +55,10 @@ sudo apt-get install -qq postgresql-${PG} -y
 
 sudo apt-get install -qq postgresql-server-dev-${PG} -y
 
-# echo 'local   all             postgres                                trust' | sudo tee /etc/postgresql/${PG}/main/pg_hba.conf > /dev/null
+
 echo 'local   all             postgres                                trust' | sudo tee /etc/postgresql/${PG}/main/pg_hba.conf > /dev/null
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+echo 'host    postgres        runner          127.0.0.1/32            trust' | sudo tee /etc/postgresql/${PG}/main/pg_hba.conf > /dev/null
 sudo cat  /etc/postgresql/${PG}/main/pg_hba.conf
 # Builds under "runner"
 # Github Actions require elevated priviledges
