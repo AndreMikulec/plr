78ea11aef18b042ef742ec113e19b5854c14879d
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-24 18:45:21 -0400

    begin trying to simplify REL_19_ source pg - plr PLR, Build, Install, and Check - MAKE SURE CHANGES WORK

--- build_install_plr_from_source_postgres_on_ubuntu_and_check.sh
+++ build_install_plr_from_source_postgres_on_ubuntu_and_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x -v -e
 
-export PATH=/usr/local/pgsql/bin:$PATH
-export USE_PGXS=1
-SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
+# sudo USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make clean
+
+sudo USE_PGXS=1 SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" PATH=/usr/local/pgsql/bin:$PATH make 
 sudo USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make install
-make installcheck || (cat regression.diffs && false)
+     USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make installcheck || (cat regression.diffs && false)
