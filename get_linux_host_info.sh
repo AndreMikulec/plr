7b3d9ddd4a3410416f597ec25032cccf92e56f73
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-24 03:17:07 -0400

    bash script bash

--- get_linux_host_info.sh
+++ get_linux_host_info.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+set -x -v -e
 
-echo "Hello"
-# uname -a
-# uname -m
-# uname -o
-# cat /proc/cpuinfo
-# cat /etc/os-release
+uname -a
+uname -m
+uname -o
+cat /proc/cpuinfo
+cat /etc/os-release
