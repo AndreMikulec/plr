0f17d2ab4115c9b06dd611ca8f23f1d101a102e3
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-24 13:24:20 -0400

    get_postgres_binaries_compiled_on_ubuntu_from_postgres.org.sh NON-snapshot

--- get_postgres_binaries_compiled_on_ubuntu_from_postgres.org.sh
+++ get_postgres_binaries_compiled_on_ubuntu_from_postgres.org.sh
@@ -32,33 +32,26 @@ export PG="$1"
 # Read about the snaphsots
 # https://wiki.postgresql.org/wiki/Apt/FAQ#Development_snapshots
 #
-# snapshots
+# snapshots (I can not find a binary package! They SHOULD BE THERE, but I can not find them.)
 # sudo add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ $(lsb_release -s -c)-pgdg-snapshot main ${PG}"
-sudo add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ $(lsb_release -s -c)-pgdg-snapshot main"
 
 # non-snapshots
-# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # check your setup using the apt-cache policy command to see if "200" shows up in the output:
-# apt-cache policy postgresql-${PG}
-# apt-cache policy postgresql
 apt-cache policy postgresql-${PG}
 
-# sudo apt-get install -qq postgresql-${PG} -y
-# sudo apt-get install -qq postgresql -y
 sudo apt-get install -qq postgresql-${PG} -y
 
-# sudo apt-get install -qq postgresql-server-dev-${PG} -y
-# sudo apt-get install -qq postgresql-server-dev -y
 sudo apt-get install -qq postgresql-server-dev-${PG} -y
-
 
 echo 'local   all             postgres                                trust' | sudo tee /etc/postgresql/${PG}/main/pg_hba.conf > /dev/null
 # Builds under "runner"
 # Github Actions require elevated priviledges
-# Ubuntu non-arm64: ubuntu-latest or ubuntu-24.04     (required)
-# Ubuntu                             ubuntu-24.04-arm (not required)
+# Ubuntu non-arm64: ubuntu-latest or ubuntu-24.04         (required elevated priviledges)
+# Ubuntu                             ubuntu-24.04-arm (not required elevated priviledges)
 # https://github.com/actions/runner-images
+sudo apt-get install -qq acl -y
 sudo setfacl -Rm u:postgres:rwx,d:u:runner:rwx /home/runner  || true
 sudo pg_ctlcluster ${PG} main reload
