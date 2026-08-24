3989d0dab5a15d305bff27d95cf0b0d5a597563f
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-24 17:38:30 -0400

    From R binaries - remove PGUSER=postgres, then "make installcheck || (cat regression.diffs && false)"

--- build_install_plr_from_binary_postgres_on_ubuntu_and_check.sh
+++ build_install_plr_from_binary_postgres_on_ubuntu_and_check.sh
@@ -5,4 +5,5 @@ sudo pg_lsclusters
 export USE_PGXS=1
 SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
 sudo USE_PGXS=1 make install
-make installcheck PGUSER=postgres || (cat regression.diffs && false)
+# make installcheck PGUSER=postgres || (cat regression.diffs && false)
+make installcheck || (cat regression.diffs && false)
