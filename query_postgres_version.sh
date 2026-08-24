ebe3702530b4ca24863e200928d8cfa3345a66d8
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-24 15:45:48 -0400

    psql -h 127.0.0.1 -d postgres -c "SELECT version();"

--- query_postgres_version.sh
+++ query_postgres_version.sh
@@ -30,5 +30,11 @@ export PATH=/usr/local/pgsql/bin:$PATH
 # psql -h 127.0.0.1 -U runner -d postgres -c "SELECT version();"
 # psql -h 127.0.0.1 -U runner -d postgres -c "SELECT current_setting('server_version_num') "server_version_num";"
 # psql: connection to server at "127.0.0.1", port 5432 failed: FATAL:  role "runner" does not exist
-psql -h 127.0.0.1 -U postgres -d postgres -c "SELECT version();"
-psql -h 127.0.0.1 -U postgres -d postgres -c "SELECT current_setting('server_version_num') "server_version_num";"
+# THIS WORKS 18 repo | REL_19_ psql: error: connection to server at "127.0.0.1", port 5432 failed: FATAL:  role "postgres" does not exist
+# psql -h 127.0.0.1 -U postgres -d postgres -c "SELECT version();"
+# psql -h 127.0.0.1 -U postgres -d postgres -c "SELECT current_setting('server_version_num') "server_version_num";"
+
+psql -h 127.0.0.1 -d postgres -c "SELECT version();"
+psql -h 127.0.0.1 -d postgres -c "SELECT current_setting('server_version_num') "server_version_num";"
+
+
