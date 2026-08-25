0431720a43745bdab1776324124620121e8040f3
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-25 19:11:56 -0400

    REL_19_ Of PLR, Build, Install, and Check - verify

--- build_install_plr_from_source_postgres_on_ubuntu_and_check.sh
+++ build_install_plr_from_source_postgres_on_ubuntu_and_check.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -x -v -e
 
-# sudo USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make clean
-sudo   USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make 
-sudo   USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make install
-       USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make installcheck || (cat regression.diffs && false)
+# sudo   USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make 
+# sudo   USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make install
+#        USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make installcheck || (cat regression.diffs && false)
+       
+# # sudo USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make clean
+
+PATH=/usr/local/pgsql/bin:$PATH
+
+USE_PGXS=1 SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make 
+sudo --preserve-env=PATH USE_PGXS=1 make install
+                         USE_PGXS=1 make installcheck || (cat regression.diffs && false)
+
+# USE_PGXS=1  make clean
