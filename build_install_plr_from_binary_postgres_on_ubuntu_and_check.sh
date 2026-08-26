184af59b2f03fdd47ef0046763da808f575b7b90
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-25 20:19:01 -0400

    repo 18 - PLR, Build, Install, and Check - full run-through

--- build_install_plr_from_binary_postgres_on_ubuntu_and_check.sh
+++ build_install_plr_from_binary_postgres_on_ubuntu_and_check.sh
@@ -8,15 +8,22 @@ set -x -v -e
 # # make installcheck PGUSER=postgres || (cat regression.diffs && false)
 # make installcheck || (cat regression.diffs && false)
 
-sudo pg_lsclusters
 
-                 chmod 777 -R /home/runner/work/plr/plr || true
-sudo -u postgres chmod 777 -R /home/runner/work/plr/plr || true
-sudo             chmod 777 -R /home/runner/work/plr/plr || true
+# sudo pg_lsclusters
+# 
+#                  chmod 777 -R /home/runner/work/plr/plr || true
+# sudo -u postgres chmod 777 -R /home/runner/work/plr/plr || true
+# sudo             chmod 777 -R /home/runner/work/plr/plr || true
+# 
+# USE_PGXS=1 make clean
+# USE_PGXS=1 SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
+# # "install" can not read environment variables nor pre-sudo variables
+# sudo USE_PGXS=1 make install
+# USE_PGXS=1 make installcheck || (cat regression.diffs && false)
 
-USE_PGXS=1 make clean
+
 USE_PGXS=1 SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
-# "install" can not read environment variables nor pre-sudo variables
 sudo USE_PGXS=1 make install
-USE_PGXS=1 make installcheck || (cat regression.diffs && false)
+     USE_PGXS=1 make installcheck || (cat regression.diffs && false)
 
+# USE_PGXS=1 make clean
