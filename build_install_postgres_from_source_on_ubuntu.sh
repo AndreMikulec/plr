a9a817d22287b48718fc2ef7d1110ae28f0a8188
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-31 02:27:42 -0400

    Try Ubuntu PG from source "meson" setup, compile, install" 3

--- build_install_postgres_from_source_on_ubuntu.sh
+++ build_install_postgres_from_source_on_ubuntu.sh
@@ -65,7 +65,7 @@ pushd postgres
 sudo apt-get install -qq python3 ninja-build meson -y
 meson setup -Dbuildtype=release -Ddebug=false -Db_pie=true -Dnls=disabled -Dplperl=disabled -Dplpython=disabled -Dpltcl=disabled -Dicu=disabled -Dllvm=disabled -Dlz4=disabled -Dzstd=disabled -Dgssapi=disabled -Dldap=disabled -Dpam=disabled -Dbsd_auth=disabled -Dsystemd=disabled -Dbonjour=disabled -Dlibxml=disabled -Dlibxslt=disabled -Dreadline=enabled -Dzlib=disabled -Ddocs=disabled -Ddocs_pdf=disabled -Dcassert=false -Dtap_tests=disabled -Db_coverage=false -Ddtrace=disabled build
 meson compile -C build -v
-meson install -C build
+sudo meson install -C build
 popd
 
 # 30 seconds long
