dda264d5f8840986224d4261a10f96489597c62e
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-31 04:50:24 -0400

    Try Ubuntu PLR from PG  source "meson" setup, compile, MANUAL regression test" TRY 4

--- build_install_plr_from_source_postgres_on_ubuntu_and_check.sh
+++ build_install_plr_from_source_postgres_on_ubuntu_and_check.sh
@@ -4,24 +4,25 @@ set -x -v -e
 # sudo   USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
 # sudo   USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make install
 #        USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make installcheck || (cat regression.diffs && false)
-
-# # sudo USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make clean
-
-##### THIS ALL WORKS
+####
+#### # # sudo USE_PGXS=1 PATH=/usr/local/pgsql/bin:$PATH make clean
+####
+####
 #### PATH=/usr/local/pgsql/bin:$PATH
 #### # could "export PATH" help ?
-####
+#### 
 #### USE_PGXS=1 SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
 #### sudo --preserve-env=PATH USE_PGXS=1 make install
 ####                          USE_PGXS=1 make installcheck || (cat regression.diffs && false)
 ####
 #### # USE_PGXS=1  make clean
+####
 
 # libR.pc THAT COMES WITH "libR R INSTALLED ON UBUNTU"
 # export PKG_CONFIG_PATH=$(pwd):${PKG_CONFIG_PATH}
 
-# installed from source ( pg_config and psql )
-export PG_HOME=/usr/local/pgsql
+               
+export PG_HOME=$(dirname "$(pg_config | grep "^INCLUDEDIR " | sed "s/ = /=/" | sed "s/^.*=//")")
 export PATH=${PG_HOME}/lib:${PATH}
 export PATH=${PG_HOME}/bin:${PATH}
 export R_HOME=$(pkg-config --variable=rhome libR)
