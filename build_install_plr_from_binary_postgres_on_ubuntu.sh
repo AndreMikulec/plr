080df8fc3912680a1e15ecce80561dff13623e05
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-09-08 23:50:12 -0400

    USE_PGXS=1 LCOVFLAGS="-q --legend --ignore-errors usage,empty" SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make

--- build_install_plr_from_binary_postgres_on_ubuntu.sh
+++ build_install_plr_from_binary_postgres_on_ubuntu.sh
@@ -10,5 +10,5 @@ unset R_HOME
 
 # SHLIB_LINK PG_CPPFLAGS
 # https://www.postgresql.org/docs/current/extend-pgxs.html
-USE_PGXS=1 SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
+USE_PGXS=1 LCOVFLAGS="-q --legend --ignore-errors usage,empty" SHLIB_LINK=-lgcov PG_CPPFLAGS="-fprofile-arcs -ftest-coverage -O0" make
 sudo USE_PGXS=1 make install
